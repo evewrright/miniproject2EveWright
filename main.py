2f797c808c742c80326b4a3f46c85048f14e1735
Eve Wright <e_wright5@mail.fhsu.edu>
2024-02-25 14:53:38 -0600

    Added block that finds rows with specific mother and father qualifications and then finds mean first semester grade for that group

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-student_info = pd.read_csv("data/dataset.csv", index_col=0, parse_dates=True)
+students = pd.read_csv("dataset.csv", index_col=0)
+
+# Find students/rows where either mother or father graduated high school AND student was enrolled in >0 units first sem
+myinfo = students[(students["Mother's qualification"] == 1) | (students["Father's qualification"] == 1) & (
+students["Curricular units 1st sem (enrolled)"] > 0)]
+
+# For those students/rows filtered above, find average of first sem avg grade
+avg_grade = myinfo['Curricular units 1st sem (grade)'].mean()
+print(avg_grade)
