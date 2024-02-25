6db0d5b630a074a5cae1c87957dca3ec83aa959f
Eve Wright <e_wright5@mail.fhsu.edu>
2024-02-25 15:12:53 -0600

    Finished function "grade1ByQuals" to filter by parent qual and find avg grade first semester

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ students["Curricular units 1st sem (enrolled)"] > 0)]
 
 # Function that takes a list of the parent qualifications (quals) by which you are trying to filter and finds all rows
 # where either Mother OR Father has one of those qualifications AND where student is enrolled in >0 units 1st sem
-define filtByQua
-students[(students["Mother's qualification"].isin(quals)) | (students["Father's qualification"].isin(
-quals)) & (students["Curricular units 1st sem (enrolled)"] > 0)]
+def grade1ByQuals(quals):
+    filtGroup = students[(students["Mother's qualification"].isin(quals)) | (students["Father's qualification"].isin(
+    quals)) & (students["Curricular units 1st sem (enrolled)"] > 0)]
+    avgGrade = filtGroup['Curricular units 1st sem (grade)'].mean()
+    return avgGrade
 
-highschool
-bachelors
-masters
-doctorate
+avgGrade_MS = grade1ByQuals([9, 10, 13, 14, 19, 29])
+avgGrade_HS = grade1ByQuals([1])
+avgGrade_Bach = grade1ByQuals([2])
+avgGrade_Mast = grade1ByQuals([4, 43])
+avgGrade_Doc = grade1ByQuals([5, 44])
 
 # For those students/rows filtered above, find average of first sem avg grade
 avg_grade = myinfo['Curricular units 1st sem (grade)'].mean()
