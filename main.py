e77854b2b193e371e921681c02792ffd0658f297
Eve Wright <e_wright5@mail.fhsu.edu>
2024-02-27 17:34:05 -0600

    Adjusted qualifications lists for middle school

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def grade1ByAge(ages):
 
 
 # Find Avg 1st Semester Grades by At Least 1 Parent Qualification
-avgGrade_MS = grade1ByQuals([9, 10, 13, 14, 19, 29])
+avgGrade_MS = grade1ByQuals([9, 10, 12, 13, 14, 19, 29, 30])
 avgGrade_HS = grade1ByQuals([1])
 avgGrade_Bach = grade1ByQuals([2])
 avgGrade_Mast = grade1ByQuals([4])
@@ -62,7 +62,7 @@ plt.show()
 
 
 # Find Avg 2nd Semester Grades by At Least 1 Parent Qualification
-avgGrade2_MS = grade2ByQuals([9, 10, 13, 14, 19, 29])
+avgGrade2_MS = grade2ByQuals([9, 10, 12, 13, 14, 19, 29, 30])
 avgGrade2_HS = grade2ByQuals([1])
 avgGrade2_Bach = grade2ByQuals([2])
 avgGrade2_Mast = grade2ByQuals([4])
@@ -79,13 +79,14 @@ plt.show()
 
 
 # Find Avg 1st Semester Grade by Parent Qualification of BOTH parents
+grade1Both_MS = grade1Both([1, 2, 4, 5, 9, 10, 12, 13, 14, 19, 29, 30], [9, 10, 12, 13, 14, 19, 29, 30])
 grade1Both_HS = grade1Both([1, 2, 4, 5], [1])
 grade1Both_Bach = grade1Both([2, 4, 5], [2])
 grade1Both_Mast = grade1Both([4, 5], [4])
 grade1Both_Doc = grade1Both([5], [5])
 
-qualsBoth = ['High School', "Bachelor's", "Master's", "Doctorate"]
-avgGrades1Both = [grade1Both_HS, grade1Both_Bach, grade1Both_Mast, grade1Both_Doc]
+qualsBoth = ['Middle School', 'High School', "Bachelor's", "Master's", "Doctorate"]
+avgGrades1Both = [grade1Both_MS, grade1Both_HS, grade1Both_Bach, grade1Both_Mast, grade1Both_Doc]
 
 plt.bar(qualsBoth, avgGrades1Both, color='purple')
 plt.ylim(min(avgGrades1Both) - .5, max(avgGrades1Both) + .5)
@@ -96,12 +97,13 @@ plt.show()
 
 
 # Find Avg 2nd Semester Grade by Parent Qualification of BOTH parents
+grade2Both_MS = grade2Both([1, 2, 4, 5, 9, 10, 12, 13, 14, 19, 29, 30], [9, 10, 12, 13, 14, 19, 29, 30])
 grade2Both_HS = grade2Both([1, 2, 4, 5], [1])
 grade2Both_Bach = grade2Both([2, 4, 5], [2])
 grade2Both_Mast = grade2Both([4, 5], [4])
 grade2Both_Doc = grade2Both([5], [5])
 
-avgGrades2Both = [grade2Both_HS, grade2Both_Bach, grade2Both_Mast, grade2Both_Doc]
+avgGrades2Both = [grade2Both_MS, grade2Both_HS, grade2Both_Bach, grade2Both_Mast, grade2Both_Doc]
 
 plt.bar(qualsBoth, avgGrades2Both, color='purple')
 plt.ylim(min(avgGrades2Both) - .5, max(avgGrades2Both) + .5)
@@ -128,21 +130,25 @@ plt.ylabel('Average 2nd Semester Grade')
 plt.show()
 
 
-
+'''
 # Find which parental occupations have highest average grades 1st semester
 newGroup = students[students["Curricular units 1st sem (approved)"] > 0]
 momOcc = newGroup.groupby(["Mother's occupation"])['Curricular units 1st sem (grade)'].mean()
 dadOcc = newGroup.groupby(["Father's occupation"])['Curricular units 1st sem (grade)'].mean()
-bothOcc = newGroup.groupby(["Father's occupation", "Mother's occupation"])['Curricular units 1st sem (grade)'].mean()
-print(dadOcc)
-print(momOcc.head())
-'''
+bothOcc = newGroup.groupby(["Father's occupation", "Mother's occupation"])['Curricular units 1st sem (grade)'].mean().reset_index()
+#print(dadOcc)
+#print(momOcc)
+#print(bothOcc)
+-----
+occupation = newGroup["Father's occupation"].unique()
+occupationM = newGroup["Mother's occupation"].unique()
+plt.bar(occupationM, momOcc, color='red')
+plt.bar(occupation, dadOcc, color='blue')
+plt.show()
+---------
 momOcc.plot(kind='bar', color='red')
 plt.ylim(momOcc.min(), momOcc.max())
 plt.show()
 dadOcc.plot(kind='bar', color='blue')
 #plt.bar(momOcc["Mother's occupation"], momOcc['Curricular units 1st sem (grade)'])
 '''
-
-
-
